258f6a3397446fe204fe245f4640b61aa9fcbda3
Andy Worth <AndyWorth@Flywheel.io>
2019-12-08 15:46:16 -0500

    initial dev starting to work

---
+++ Dockerfile
@@ -0,0 +1,65 @@
+# editme: change this file as needed
+# Use the latest Python 3 docker image
+FROM bids/freesurfer:v6.0.1-5
+
+MAINTAINER Flywheel <support@flywheel.io>
+
+RUN curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+
+RUN apt-get update && \
+    apt-get install -y \
+    nodejs \
+    zip && \
+    rm -rf /var/lib/apt/lists/* 
+# The last line above is to help keep the docker image smaller
+
+RUN npm install -g bids-validator@1.3.12
+
+RUN apt-get  update && \
+    apt-get install -y software-properties-common && \
+    add-apt-repository ppa:jonathonf/python-3.6
+
+# Tried this one too
+#    add-apt-repository ppa:deadsnakes/ppa
+
+RUN apt-get update && \
+    apt-get install -y python3.6 && \
+    wget https://bootstrap.pypa.io/ez_setup.py -O - | python3.6
+
+RUN curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && \
+    python3.6 get-pip.py && \
+    pip3.6 install --upgrade pip 
+
+RUN apt-get update
+
+RUN pip3.6 install virtualenv 
+
+ENV VIRTUAL_ENV=/opt/venv
+RUN python3.6 -m virtualenv --python=/usr/bin/python3.6 $VIRTUAL_ENV
+ENV PATH="$VIRTUAL_ENV/bin:$PATH"
+
+RUN pip3.6 install flywheel-sdk==10.4.1 \
+        flywheel-bids==0.8.2 && \
+    rm -rf /root/.cache/pip
+
+#    rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+# could add pybids
+# The last line above is to help keep the docker image smaller
+
+# Make directory for flywheel spec (v0)
+ENV FLYWHEEL /flywheel/v0
+WORKDIR ${FLYWHEEL}
+
+# Save docker environ
+ENV PYTHONUNBUFFERED 1
+RUN python -c 'import os, json; f = open("/tmp/gear_environ.json", "w"); json.dump(dict(os.environ), f)' 
+
+# Copy executable/manifest to Gear
+COPY manifest.json ${FLYWHEEL}/manifest.json
+COPY utils ${FLYWHEEL}/utils
+COPY run.py ${FLYWHEEL}/run.py
+
+# Configure entrypoint
+RUN chmod a+x ${FLYWHEEL}/run.py
+ENTRYPOINT ["/flywheel/v0/run.py"]
---
+++ utils/args.py
@@ -0,0 +1,144 @@
+# If you edit this file, please consider updating bids-app-template
+
+import subprocess as sp
+import os, os.path as op
+import logging
+import re
+import json
+
+from .licenses.freesurfer import find_freesurfer_license
+
+
+log = logging.getLogger(__name__)
+
+
+def get_inputs_and_args(context):
+    """
+    Process inputs, contextual values and build a dictionary of
+    key:value command-line parameter names:values These will be
+    validated and assembled into a command-line below.  
+    """
+
+    log.debug('')
+
+    # 1) Process Inputs
+
+    # This is here because one way to pass the license is by an input
+    find_freesurfer_license(context, '/opt/freesurfer/license.txt')
+
+    # 2) Process Configuration (config, rest of command-line parameters)
+    config = context.config
+    params = {}
+    for key in config.keys():
+        if key[:5] == 'gear-':  # Skip any gear- parameters
+            continue
+        # Use only those boolean values that are True
+        if type(config[key]) == bool:
+            if config[key]:
+                params[key] = True
+            # else ignore (could this cause a problem?)
+        else:
+            if len(key) == 1:
+                params[key] = config[key]
+            else:
+                if config[key] != 0:  # if zero, skip and use defaults
+                    params[key] = config[key]
+                # else ignore (could this caus a problem?)
+    
+    context.gear_dict['param_list'] =  params
+
+
+def validate(context):
+    """
+    Validate settings of the Parameters constructed.
+    Gives warnings for possible settings that could result in bad results.
+    Gives errors (and raises exceptions) for settings that are violations 
+    """
+
+    log.debug('')
+
+    param_list = context.gear_dict['param_list']
+    log.info('TODO: check param_list: ' + repr(param_list))
+
+    # TODO Must be a space-separated list of these: 
+    # {autorecon1,autorecon2,autorecon2-cp,autorecon2-wm,autorecon-pial,autorecon3,autorecon-all,all} [{autorecon1,autorecon2,autorecon2-cp,autorecon2-wm,autorecon-pial,autorecon3,autorecon-all,all} ...] Autorecon stages to run.
+    # TODO (check for others in manifest)
+
+    # TODO Must be a space-separated list of these`
+    #"parcellations": {
+    #  "default": "aparc",
+    #  "description": "{aparc,aparc.a2009s} [{aparc,aparc.a2009s} ...] Group2 option: cortical parcellation(s) to extract stats from.",
+    #  "type": "string"
+    #},
+
+    # TODO this too
+    # "measurements": {
+    #   "default": "thickness",
+    #   "description": "{area,volume,thickness,thicknessstd,meancurv,gauscurv,foldind,curvind} [{area,volume,thickness,thicknessstd,meancurv,gauscurv,foldind,curvind} ...] Group2 option: cortical measurements to extract stats for.",
+    #   "type": "string"
+    # },
+
+    # Test for input existence
+    # if not op.exists(params['i']):
+    #    raise Exception('Input File Not Found')
+
+    # Tests for specific problems/interactions that can raise exceptions or log warnings
+    # if ('betfparam' in params) and ('nononlinreg' in params):
+    #    if(params['betfparam']>0.0):
+    #        raise Exception('For betfparam values > zero, nonlinear registration is required.')
+
+    # if ('s' in params.keys()):
+    #    if params['s']==0:
+    #        log.warning(' The value of ' + str(params['s'] + \
+    #                    ' for -s may cause a singular matrix'))
+
+
+def build_command(context):
+    """
+    command is a list of prepared commands
+    param_list is a dictionary of key:value pairs to be put into the command list
+    as such ("-k value" or "--key=value")
+    """
+
+    log.debug('')
+
+    command = context.gear_dict['command_line']
+
+    param_list = context.gear_dict['param_list']
+
+    for key in param_list.keys():
+        # Single character command-line parameters are preceded by a single '-'
+        if len(key) == 1:
+            command.append('-' + key)
+            if len(str(param_list[key])) != 0:
+                # append it like '-k value'
+                command.append(str(param_list[key]))
+        # Multi-Character command-line parameters are preceded by a double '--'
+        else:
+            # If Param is boolean and true include, else exclude
+            if type(param_list[key]) == bool:
+                if param_list[key]:
+                    command.append('--' + key)
+            else:
+                # If Param not boolean, but without value include without value
+                if len(str(param_list[key])) == 0:
+                    # append it like '--key'
+                    command.append('--' + key)
+                else:
+                    # check for argparse nargs='*' lists of multiple values so
+                    #  append it like '--key val1 val2 ...'
+                    if (isinstance(param_list[key], str) and len(param_list[key].split()) > 1):
+                    # then it is a list of multiple things: e.g. "--modality T1w T2w"
+                        command.append('--' + key)
+                        for item in param_list[key].split():
+                            command.append(item)
+                    else: # single value so append it like '--key=value'
+                        command.append('--' + key + '=' + str(param_list[key]))
+        if key == 'verbose':
+            # handle a 'count' argparse argument where manifest gives
+            # enumerated possibilities like v, vv, or vvv
+            # e.g. replace "--verbose=vvv' with '-vvv'
+            command[-1] = '-' + param_list[key]
+
+
+# vi:set autoindent ts=4 sw=4 expandtab : See Vim, :help 'modeline'
